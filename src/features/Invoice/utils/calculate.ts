d16b0a36986ecedbda0fb2f2da4155972aac20a3
Roselle Tabuena <roselleroxtabuena@gmail.com>
2024-11-09 00:12:18 +0800

    chore, fix: remove console log; ../types/invoiceTypes has no exported member named InvoiceTableProps

--- src/features/Invoice/utils/calculate.ts
+++ src/features/Invoice/utils/calculate.ts
@@ -11,23 +11,7 @@ export const calculateAmount = (
   unit: string,
   unit_price: number
 ) => {
-  // Ensure that the unit exists in the units object
   const unitFactor = units[unit];
-  if (unitFactor === undefined) {
-    console.error(`Invalid unit: ${unit}`);
-    return 0; // Return 0 or handle appropriately
-  }
-
-  // Ensure quantity and unit_price are valid numbers
-  if (isNaN(quantity) || isNaN(unit_price)) {
-    console.error(
-      `Invalid quantity or unit_price: quantity=${quantity}, unit_price=${unit_price}`
-    );
-    return 0; // Return 0 or handle appropriately
-  }
-
-  console.log(unit, unitFactor);
-
   return quantity * unitFactor * unit_price;
 };
 
