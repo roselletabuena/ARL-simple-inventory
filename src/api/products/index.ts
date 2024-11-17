b381bc0fb768f1b9ec0f2d243b1e3cf1f33fa89f
Roselle Tabuena <roselleroxtabuena@gmail.com>
2024-11-17 11:37:21 +0800

    fixes: data type for products; unathorized api on first login

--- src/api/products/index.ts
+++ src/api/products/index.ts
@@ -1,17 +1,19 @@
 import axios from "axios";
+import { Products } from "../../types/products";
 
 const ARL_ENDPOINT = import.meta.env.VITE_ARL_ENDPOINT;
 
-const headers = {
-  Authorization: sessionStorage.getItem("authToken"),
-  "Content-Type": "application/json",
-};
-
-console.log(sessionStorage.getItem("authToken"));
-
 export const getProducts = async (): Promise<any> => {
+  const headers = {
+    Authorization: sessionStorage.getItem("authToken"),
+    "Content-Type": "application/json",
+  };
+
   try {
-    const response = await axios.get<any>(ARL_ENDPOINT, { headers });
+    const response = await axios.get<{ products: Products }>(ARL_ENDPOINT, {
+      headers,
+    });
+
     return response.data.products;
   } catch (error) {
     console.error("Error fetching data: ", error);
